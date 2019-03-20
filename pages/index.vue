695e6f02bdfe3d86f357e37815586e60dd114a55
Ryo Nagaoka <ryo.nagaoka@gmail.com>
2019-03-20 10:00:48 +0900

    add default amplify settings

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <no-ssr>
+        <amplify-authenticator :auth-config="authConfig"/>
+        <amplify-sign-out/>
+      </no-ssr>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+        authConfig: {
+          // 通常のログイン
+          signInConfig: {
+            header: 'Sign in !!'
+          },
+          // 登録
+          signUpConfig: {
+            hideDefaults: true,
+            signUpFields: [
+              { label: 'Name', key: 'name', required: true, type: 'text', displayOrder: 0 },
+              { label: 'Email', key: 'username', required: true, type: 'email', displayOrder: 1 },
+              { label: 'Password', key: 'password', required: true, type: 'password', displayOrder: 2 }
+            ]
+          },
+          // 登録confirm
+          confirmSignUpConfig: {},
+          // パス忘れ
+          forgotPasswordConfig: {},
+          // MFA
+          confirmSignInConfig: {}
+        }
+      }
+    }
+  }
+</script>
